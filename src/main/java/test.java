cc151d7994d87541ab5fb3664379711aafb65777
Admin <230140@astanait.edu.kz>
2024-02-08 14:00:37 +0600

    gg

---
+++ src/main/java/test.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class test {
+    public static void main(String[] args) {
+        String connectionUrl = "jdbc:postgresql://localhost:5432/postgres";
+        Connection con = null;
+        ResultSet rs = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            con = DriverManager.getConnection(connectionUrl, "postgres", "3789");
+            stmt = con.createStatement();
+
+            rs = stmt.executeQuery("select * from student");
+
+            while (rs.next())
+                System.out.println(rs.getString("name"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
