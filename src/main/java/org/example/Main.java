d8ab127cf4c01e675fe9e68f52dc2cc0e5b4d733
Admin <230140@astanait.edu.kz>
2024-02-11 01:11:02 +0600

    preview and login

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,57 @@
 package org.example;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main extends JFrame implements ActionListener{
+
+    Main() {
+        setSize(1366, 700);
+        setLocation(100, 100);
+        ImageIcon i1 = new ImageIcon("C:\\\\items\\\\Hilton2.jpg");
+        Image i2 = i1.getImage().getScaledInstance(1366, 700, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        add(image);
+        JLabel text = new JLabel("Hilton Managment System");
+        text.setBounds(20,550,900,90);
+        text.setForeground(Color.cyan);
+        text.setFont(new Font("serif", Font.PLAIN, 50));
+        image.add(text);
+        JButton button = new JButton("Next");
+        button.setBounds(1150,565,150,50);
+        button.addActionListener(this);
+        image.add(button);
+
+        setVisible(true);
+
+        while (true){
+            text.setVisible(false);
+            try{
+                Thread.sleep(500);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+            text.setVisible(true);
+            try{
+                Thread.sleep(500);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
         }
+
+
+
+    }
+    public void actionPerformed(ActionEvent ae){
+        setVisible(false);
+        new Login();
+
+    }
+    public static void main(String[] args) {
+        new Main();
+
     }
 }
