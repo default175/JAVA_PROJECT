a7284a72f75790de5e92f07063f85ba1076026f1
zxc777zxc <153791686+zxc777zxc@users.noreply.github.com>
2024-02-27 18:48:12 +0600

    Update Main.java
    Add client remover

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 
 import java.sql.*;
-import lombok.*;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -87,6 +87,19 @@ public class Main {
                 System.out.println("Staff deleted from the database: " + staff.getName());
             } else {
                 System.out.println("Failed to delete staff from the database: " + staff.getName());
+            }
+        }
+    }
+    // Deletes a client from the database
+    public static void deleteClientFromDatabase(Connection connection, Client client) throws SQLException {
+        String sql = "DELETE FROM clients WHERE name=?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, client.getName());
+            int rowsDeleted = statement.executeUpdate();
+            if (rowsDeleted > 0) {
+                System.out.println("Client deleted from the database: " + client.getName());
+            } else {
+                System.out.println("Failed to delete client from the database: " + client.getName());
             }
         }
     }
